2d4538fe6420818b08d2d892247eebb64e7268fd
James Brett <jmsbrett@gmail.com>
2018-04-18 18:46:26 -0500

    adding components

---
+++ src/app/stock-overview/stock-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-overview',
+  templateUrl: './stock-overview.component.html',
+  styleUrls: ['./stock-overview.component.css']
+})
+export class StockOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
