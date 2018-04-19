73d7db9a6d6407980f049f58e44bc1f03ddec93c
James Brett <jmsbrett@gmail.com>
2018-04-18 19:01:48 -0500

    changes

--- src/app/stock-overview/stock-overview.component.ts
+++ src/app/stock-overview/stock-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stock-overview',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockOverviewComponent implements OnInit {
 
-  constructor() { }
+  faveStocks = [];
+  selectedNum = 0;
 
+  
+  constructor() { }
+  
   ngOnInit() {
   }
+
+  getFaveStocks() {
+    return JSON.stringify(this.faveStocks);
+  }
+  
+  @Input('addedStock')
+  set addedStock(selectedStock: any) {
+    console.log('addedStock : ' + JSON.stringify(selectedStock));
+    if (selectedStock) {
+      let stockIndex = this.faveStocks.indexOf(selectedStock.title);
+      if (stockIndex > -1) {
+        this.faveStocks.splice(stockIndex, 1);
+      } else {
+        this.faveStocks.push(selectedStock.title);
+      }
+    }
+  }
+
 
 }
